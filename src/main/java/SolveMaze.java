0ab3ef7decedb2ee9388dbcf70bf5ef1e0fd1242
BuildTools <unconfigured@null.spigotmc.org>
2018-01-31 14:26:51 -0600

    Solved the maze

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -20,7 +20,7 @@ public class SolveMaze {
         /*
          * Create a new 10 x 10 maze. Feel free to change these values.
          */
-        Maze maze = new Maze(10, 10);
+        Maze maze = new Maze(5, 5);
 
         /*
          * Pick (0, 0), the bottom left corner, as the starting point.
@@ -33,8 +33,15 @@ public class SolveMaze {
          * You should be able to solve a 10 x 10 maze in (far fewer than) 1000 steps.
          * Feel free to adjust this number if you experiment with other mazes.
          */
-        for (int step = 0; step < 1000; step++) {
-            // Implement your maze solving algorithm here
+        for (int step = 0; step < 1000000; step++) {
+            System.out.println();
+            System.out.println(step);
+            System.out.println(maze);
+            maze.turnRight();
+            while (maze.canMove() == false) {
+                maze.turnLeft();
+            }
+            maze.move();
         }
 
         if (maze.isFinished()) {
